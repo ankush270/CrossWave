d7d8ed76ad2c1550e91747bedc2aa42fe14e5f9b
Anshul Gupta <anshulgupta8854@gmail.com>
2025-01-30 22:10:26 +0530

    review controller

--- backend/src/microservices/Anshul/routes/review.routes.js
+++ backend/src/microservices/Anshul/routes/review.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { avgRating, getReviews, review } from "../controllers/review.controller.js";
+import { getProductReviews, getUserReviews, productAvgRating, productReview, userAvgRating, userReview } from "../controllers/review.controller.js";
 
 const router = Router();
 
-router.route('/review/:reviewee_id').post(review);
-router.route('/get-reviews/:reviewee_id').get(getReviews);
-router.route('/avg-rating/:reviewee_id').get(avgRating);
+router.route('/user-review/:reviewee_id').post(userReview);
+router.route('/get-user-reviews/:reviewee_id').get(getUserReviews);
+router.route('/user-avg-rating/:reviewee_id').get(userAvgRating);
+router.route('/product-review/:product_id').post(productReview);
+router.route('/get-product-reviews/:product_id').get(getProductReviews);
+router.route('/product-avg-rating/:product_id').get(productAvgRating);
 
 export default router;
