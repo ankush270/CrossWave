4b37e2951ced0ee2803043d41969c24ef39ed031
ankush270 <ankushchoudhary2700@gmail.com>
2025-02-03 19:14:15 +0530

    com

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-// import 'dotenv/config';
+import 'dotenv/config';
 import express from "express";
 import prisma from "./src/config/prisma_db.js";
 import connectMongoDB from "./src/config/mongo_db.js";
