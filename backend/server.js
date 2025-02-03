d3030812cc4987420dd2a2fa983d24a9d3e4b3ad
RohitAnandNitk <sharmarohit64748@gmail.com>
2025-02-03 18:31:49 +0530

    committed

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,11 @@ import profileRouter from "./src/routes/profile.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import kycRouter from "./src/routes/kyc.js";
-import { extractText } from "./src/microservices/kyc/aadhaar.js";
+// import { extractText } from "./src/microservices/kyc/aadhaar.js";
 // payment routes
 import PaymentRoutes from "./src/routes/PaymentRoutes.js";
+// chat routes
+import ChatRoutes from "./src/routes/ChatRoutes.js";
 
 import {
   verifyProduct,
@@ -23,12 +25,25 @@ const PORT = process.env.PORT || 3000;
 import dotenv from "dotenv";
 dotenv.config();
 
+
+// Import socket middleware and http
+import { initializeSocket } from "./src/middlewares/socketio.js";
+import http from "http";
+const ioserver = http.createServer(app); // HTTP server
+// Initialize Socket.IO
+initializeSocket(ioserver);
+
+
+
+
+
 //Connect with MongoDB
 connectMongoDB();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
@@ -40,14 +55,17 @@ app.use(express.urlencoded({ extended: true })); // Parse form data
 app.use("/user", userRouter);
 app.use("/kyc", kycRouter);
 app.post("/verify-product", upload.array("files", 10), verifyProduct);
-app.post("/extract-text", extractText);
+// app.post("/extract-text", extractText);
 app.use("/user", userRouter);
 app.use("/profile", profileRouter);
 app.use("/payment", PaymentRoutes);
+app.use('/chat', ChatRoutes);
+
 
 // Start server
-const server = app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const SOCKETIO_PORT = PORT;
+const server = app.listen(SOCKETIO_PORT, () => {
+  console.log(`Server running on http://localhost:${SOCKETIO_PORT}`);
 });
 
 // Graceful shutdown
